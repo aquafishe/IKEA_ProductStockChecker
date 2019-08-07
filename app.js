55e6b404171929a1c23402785468ae86b410fe3a
aquafishe <austen.d.young@gmail.com>
2019-08-06 22:36:09 -0400

    Adding node.js

---
+++ app.js
@@ -0,0 +1,103 @@
+var request = require("request");
+var express = require("express");
+var parseString = require('xml2js').parseString;
+var nodemailer = require('nodemailer');
+
+var productId = 60464407;
+
+//var productId = "S69009475"; // IN STOCK TEST PRODUCT
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'austeny.bot@gmail.com',
+        pass: 'lemonade117'
+    }
+});
+
+const mailOptions = {
+    from: 'austeny.bot@gmail.com', // sender address
+    to: 'austen.d.young@gmail.com', //carolinetelma97@gmail.com', // list of receivers
+    subject: 'Default Header', // Subject line
+    html: '<h2 style="display: inline;">IS SLATTUM BED IN STOCK? </h2>'// plain text body
+};
+
+
+console.log("starting ikea bot");
+var checkCount = 0;
+
+var intervalId = setInterval(function(){
+    checkCount += 1;
+
+    checkInStock(productId);
+    if (inStockIndicator){
+        console.log("item found");
+        clearInterval(intervalId);
+        sendEmail();
+    }
+
+    if (checkCount == 30){
+        checkCount = 0;
+        sendEmail();
+    }
+
+    console.log("checkCount: " + checkCount);
+    console.log(mailOptions);
+}, 3000);
+
+function sendEmail() {
+    console.log("sending email");
+    console.log(mailOptions);
+    transporter.sendMail(mailOptions, function (err, info) {
+        if(err){
+            console.log(err)
+        } else {
+            console.log(info);
+        };
+    });
+}
+
+var inStockIndicator = false;
+
+function checkInStock(productId){
+    var url = "https://www.ikea.com/us/en/iows/catalog/availability/" + productId;
+    
+    request(url, function(error, response, body){
+        if(!error && response.statusCode === 200){
+            parseString(body, function(err, result){
+                console.log("successful request call");
+
+                numOfStores = result['ir:ikea-rest'].availability[0].localStore.length;
+    
+                for(var i=0; i<numOfStores; i++){
+                    if(result['ir:ikea-rest'].availability[0].localStore[i].$.buCode == 158){
+                        var storeJson = result['ir:ikea-rest'].availability[0].localStore[i].stock[0];
+                        var currentStock = storeJson.availableStock[0];
+                        var inStockProbability = storeJson.inStockProbabilityCode[0];
+    
+                        if (currentStock > 0){
+                            mailOptions.subject = `IN STOCK, GO ORDER!!! qty: ${currentStock}`;
+                            mailOptions.html += `<h1 style="display: inline; font-size: 50px; color: lime;">YES!</h1>
+                                                <h3>QTY: <span style="color: lime;">${currentStock}</span><br>
+                                                IN-STOCK PROBABILITY: <span style="color: lime;">${inStockProbability}</span></h3>
+                                                <a href="https://www.ikea.com/us/en/catalog/products/60464407/">Link to IKEA product page</a>`;
+                            inStockIndicator = true;
+                        }else{
+                            mailOptions.subject = "not in stock...";
+                            mailOptions.html += `<h1 style="display: inline; font-size: 50px; color: indianred;">NOPE...</h1>
+                                                <h3>QTY: <span style="color: indianred;">${currentStock}</span><br>
+                                                IN-STOCK PROBABILITY: <span style="color: indianred;">${inStockProbability}</span></h3>
+                                                <a href="https://www.ikea.com/us/en/catalog/products/60464407/">Link to IKEA product page</a>`;
+                        }        
+                    }
+                }
+    
+            })
+    
+        } else {
+            console.log(error);
+            console.log("Status Code: " + response.statusCode);
+        }
+    })
+}
+
